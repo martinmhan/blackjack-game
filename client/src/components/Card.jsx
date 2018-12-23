b912a6b9caeb0af7854728a4751743fedf2d2ba6
Martin Han <martinhan@martins-mbp-2.attlocal.net>
2018-12-22 22:20:07 -0800

    refactored Card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
-const Card = (props) => {
-  let imgPath = `images/${props.card}.jpg`;
-  if (props.gameStatus === 'in play' && props.index === 0) {
-    return (
-      <div className="card">
-        <img src="images/blue_back.jpg"></img>  
-      </div>
-    );
-  } else {
-    return (
-      <div className="card">
-        <img src={imgPath}></img>
-      </div>
-    );
-  }
-};
+const Card = (props) => (
+  props.gameStatus === 'in play' && props.index === 0 ? 
+    <div className="card">
+      <img src="images/blue_back.jpg"></img>
+    </div> :
+    <div className="card">
+      <img src={`images/${props.card}.jpg`}></img>
+    </div>
+);
 
 export default Card;
